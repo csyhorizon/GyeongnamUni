193fa113663124a45a9e30cae23ebd2f31185335
chinoel <tnehd83@naver.com>
2024-11-20 12:32:38 +0900

    feat: 집랭 언어 만들기 시작

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String inputValue = "Please\nCheck\nData";
+    }
+}
