e49dd8be59d33b1069a52f53ab01dca7b7243dcc
new010330 <rlarudtjs74a@naver.com>
2025-07-29 16:16:28 +0900

    image code update

--- skin_status/face_analysis/views.py
+++ skin_status/face_analysis/views.py
@@ -41,7 +41,7 @@ def crop_face_from_image(image: np.ndarray) -> np.ndarray:
 @parser_classes([MultiPartParser])
 def analyze_faces_and_acne_level(request):
     images = request.FILES.getlist('image')
-    if not images or len(images) != 5:
+    if not images or len(images) >= 5:
         return Response({'detail': '이미지 5장이 필요합니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
     upload_dir = os.path.join(settings.MEDIA_ROOT, 'uploaded')
